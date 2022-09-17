ed8ffcbf51e86b0d79d7c0ea6ee3b91010cbb0dc
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-09-17 18:50:48 +0300

    inicializate create page and create busnes logic

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,13 +1,37 @@
 const { isUser } = require("../middleware/guards");
+const { createTrip } = require("../services/tripService");
+const mapErrors = require("../util/mapers");
 
 const router = require(`express`).Router();
 
 router.get(`/create`,isUser(), (req, res) => {
-     res.render(`create`, {title: `Create Trip Ofer`});
+     res.render(`create`, {title: `Create Trip Ofer`, data: {}});
 });
 
-router.post(`/create`, isUser(), (req, res) => {
-    res.redirect(`/trips`);
+router.post(`/create`, isUser(), async(req, res) => {
+    const trip = {
+        start: req.body.start,
+        end: req.body.end,
+        date: req.body.date,
+        time: req.body.time,
+        carImg: req.body.carImg,
+        carBrand: req.body.carBrand,
+        seats: req.body.seats,
+        price: req.body.price,
+        description: req.body.description,
+        owner: req.session.user._id
+    }
+
+    try {
+
+        await createTrip(trip);
+        res.redirect(`/trips`);
+        
+    } catch (err) {
+        console.error(err);
+        const errors = mapErrors(err);
+        res.render(`create`, { title: `Create Trip Ofer` ,data: trip, errors});
+    }
 });
 
 
